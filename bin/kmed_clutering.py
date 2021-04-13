c7acb37c581d8972370a15fda3faf867c4bc9d96
Caitlin Simopoulos <csimopou@uOttawa.ca>
2021-04-13 18:32:32 -0400

    Adding option to set threads for k-med clustering

--- bin/kmed_clutering.py
+++ bin/kmed_clutering.py
@@ -22,7 +22,7 @@ def compare_k_AggClustering(k_list, X):
     # Run clustering with different k and check the metrics
     silhouette_list = []
     for p in k_list:
-        print("Calculating silhouette score for k=", p)
+        print("Calculating silhouette score for k =", p)
         clusters = AgglomerativeClustering(n_clusters=p, affinity='precomputed', linkage='average', distance_threshold=None)
         clusters.fit(X)
         # The higher (up to 1) the better
@@ -31,7 +31,7 @@ def compare_k_AggClustering(k_list, X):
     # The higher (up to 1) the better
     key = silhouette_list.index(max(silhouette_list))
     k = k_list.__getitem__(key)
-    print("AggClust best silhouette =", max(silhouette_list), " for k=", k)
+    print("AggClust best silhouette =", max(silhouette_list), " for k =", k)
     return k, silhouette_list
 
 
@@ -41,8 +41,7 @@ def compare_k_med(k_list, X):
     silhouette_list = []
     inertia_list = []
     for p in k_list:
-        print("Calculating silhouette score for k=", p)
-        #clusters = AgglomerativeClustering(n_clusters=p, affinity='precomputed', linkage='average', distance_threshold=None)
+        print("Calculating silhouette score for k =", p)
         clusters = KMedoids(n_clusters=p, metric='precomputed', random_state=2248, init='k-medoids++')
         clusters.fit(X)
         # The higher (up to 1) the better
@@ -53,41 +52,35 @@ def compare_k_med(k_list, X):
     # The higher (up to 1) the better
     key = silhouette_list.index(max(silhouette_list))
     k = k_list.__getitem__(key)
-    print("Kmed best silhouette =", max(silhouette_list), " for k=", k)
+    print("Kmed best silhouette =", max(silhouette_list), " for k =", k)
     return k, silhouette_list, inertia_list
 
 ####################################################
 ### SET UP CMND LINE OPTIONS FOR FILE IN/OUTPUT ###
 ####################################################
 
-usage = "usage: %prog [options] -s s_mat.csv -d directory/"
+usage = "usage: %prog [options] -s s_mat.csv -n nthreads -d directory/"
 
 parser = argparse.ArgumentParser(description="Peptide/feature clusters from pre-computed ICA decomposition")
 parser.add_argument("-s", "--s_mat", dest="s_mat", required=True, metavar="S.csv", help="S matrix ICA output")
 parser.add_argument("-d", "--dir", dest="dir", required=True, metavar="directory/", help="directory for your output files")
+parser.add_argument("-n", "--threads", dest="n_threads", default=1, metavar="nthreads/", help="number of threads to use")
 args = parser.parse_args()
 
 
 
 ## data input files
-#s_mat =  "../output/S.csv"
 s_mat = args.s_mat
-#a_mat = "../output/A.csv"
-
-
 output_dir = os.path.dirname(args.dir)
-print(output_dir)
+
 ## data output files
-#aggclust_labels = os.path.join(output_dir+"aggclust_labels_pep.csv")
 kmed_labels = os.path.join(output_dir + "/k-med_labels.csv")
-print(kmed_labels)
 sil_kmed = os.path.join(output_dir + "/k-med_silhouette.csv")
-#sil_aggclust = os.path.join(output_dir+"../output/aggclust_silhouette_pep.csv")
 inertia_kmed = os.path.join(output_dir + "/k-med_inertia.csv")
-#mixing_cor_file = "../output/mixing_cor_matrix.csv"
+
 
 ## import the normalized and transformed ms1 features
-#log_features = pd.read_csv(log_features_file, header = 0)
+
 
 ### ICA
 # https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.FastICA.html
@@ -103,8 +96,10 @@ inertia_kmed = os.path.join(output_dir + "/k-med_inertia.csv")
 S_ = pd.read_csv(s_mat, header=0, index_col=0)
 #A_ = pd.read_csv(a_mat, header=0)
 
+nthreads = int(args.n_threads)
+
 ## Compute distance matrix for mixing 
-mixing_cor = pairwise_distances(S_, Y=None, metric='correlation', n_jobs=10) ##  increase the njobs if run on beter server
+mixing_cor = pairwise_distances(S_, Y=None, metric='correlation', n_jobs=nthreads) ##  increase the njobs if run on beter server
 
 ## Trying Kmedoids
 ## see which k has best silhouette score
